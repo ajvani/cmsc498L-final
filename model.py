46978874f519600a0d0a147f455c7dd300adf125
ajvani <anmol.jadvani@gmail.com>
2019-05-06 19:10:01 -0400

    add test

--- model.py
+++ model.py
@@ -1,20 +1,28 @@
 import keras
 
 class o2mCycleGAN():
-    def __init__(self, atts, data_dir='./data'):
+    def __init__(self, atts=None, data_dir='./data'):
+        default_atts = ['Bald', 'Mustache', 'Eyeglasses', 'Bangs']
+        if atts == None:
+            self.atts = default_atts
+        else:
+            self.atts = atts
+
+        self.n_atts = len(self.atts)
+        self.data_dir = data_dir
         self.img_shape = (64, 64, 3)
         self.d_out_shape = (64 / 2**3, 64 / 2**3, 1)
 
         optimizer = keras.optimizers.Adam(0.0001, 0.5)
 
         # truth -> fake discriminators
-        self.d_TF_lst = [self.__discriminator__() for _ in atts]
+        self.d_T_lst = [self.__discriminator__() for _ in atts]
 
         # fake -> truth discriminators
-        self.d_FT_lst = [self.__discriminator__() for _ in atts]
+        self.d_F_lst = [self.__discriminator__() for _ in atts]
 
         # disriminator A and disriminator B
-        for (disc_TF, disc_FT) in zip(self.d_TF_lst, self.d_FT_lst):
+        for (disc_TF, disc_FT) in zip(self.d_T_lst, self.d_F_lst):
             disc_TF.compile(loss='mse', optimizer=optimizer, metrics=['accuracy'])
             disc_FT.compile(loss='mse', optimizer=optimizer, metrics=['accuracy'])
 
@@ -40,8 +48,8 @@ class o2mCycleGAN():
         regen_F_lst = [g_FT(gen_T) for (g_FT, gen_T) in zip(self.g_FT_lst, gen_T_lst)]
 
         # check if fake images are 'real'
-        class_gen_T_lst = [d_T(gen_T) for (d_T, gen_T) in zip(self.d_TF_lst, gen_T_lst)]
-        class_gen_F_lst = [d_F(gen_T) for (d_F, gen_F) in zip(self.d_FT_lst, gen_F_lst)]
+        class_gen_T_lst = [d_T(gen_T) for (d_T, gen_T) in zip(self.d_T_lst, gen_T_lst)]
+        class_gen_F_lst = [d_F(gen_T) for (d_F, gen_F) in zip(self.d_F_lst, gen_F_lst)]
 
         # build model with all components
         self.GANs = [ 
@@ -51,7 +59,9 @@ class o2mCycleGAN():
                 for (img_A, img_B, class_gen_A, class_gen_B, regen_A, regen_B, selfgen_A, selfgen_B)
                 in zip(self.input_T_lst, self.input_F_lst, class_gen_T_lst, class_gen_F_lst,
                     regen_T_lst, regen_F_lst, selfgen_T_lst, selfgen_F_lst)]
-        self.GAN.compile(
+
+        for GAN in self.GANs:
+            GAN.compile(
                 loss=['mse' for i in range(6)],
                 loss_weights=[1,1,10,10,1,1],
                 optimizer=optimizer)
@@ -92,6 +102,7 @@ class o2mCycleGAN():
 
         return Model(init_layer, img_gen)
 
+    # discriminator: checks if input is part of class <att> or not
     def __discriminator__(self):
         # input layer
         FILTERS = 64 
@@ -111,3 +122,78 @@ class o2mCycleGAN():
         out_layer = Conv2D(1, strides=1, padding='same')(conv3)
 
         return Model(init_layer, out_layer)
+
+    def build_data(self, method='train'):
+        data_loder = CelebDataLoader(self.data_dir, self.atts, method=method)
+        data_it = data_laoder.__iterator__()
+        _img, _label = it.get_next()
+        session = tf.Session()
+
+        data = {}
+        for label_i in range(self.n_atts):
+            data[label_i] = []
+
+        while True:
+            try:
+                _img, _label = data.it.get_next()
+            except tf.errors.OutOfRangeError:
+                break
+            img, label = _img.eval(session=session), _label.eval(session=session)
+
+            for (i,cls) in enumerate(label):
+                if cls == 1:
+                    data[i].append(img)
+        return data
+        
+        
+
+    def train(self, epochs=50, batch_size=1):
+        data = self.build_data()
+
+        valid = np.ones((batch_size,) + self.d_out_shape)
+        fake = np.zeros((batch_size,) + self.d_out_shape)
+        for epoch in range(epochs):
+            for label in data.keys():
+                d_T = self.d_T_lst[label]
+                d_F = self.d_F_lst[label]
+                g_TF = self.g_TF_lst[label]
+                g_FT = self.g_FT_lst[label]
+
+                corr_att = self.atts[label]
+
+                # for label i, generate fake for all labels != i
+                img_A = data[label]
+                img_B = []
+                for fake_label in range(self.n_atts):
+                    if fake_labe == label: continue
+                    img_B += data[fake_label]
+
+                fake_F = g_TF.predict(img_A)
+                fake_T = g_FT.predict(img_B)
+
+                dT_loss_real = d_T.tain_on_batch(img_A, valid)
+                dT_loss_fake = d_T.train_on_batch(fake_T, fake)
+                dT_loss = 0.5 * np.add(dT_loss_real, dT_loss_fake)
+
+                dF_loss_real = d_F.train_on_batch(img_B, valid)
+                dF_loss_fake = d_T.train_on_batch(fake_F, fake)
+                dF_loss = 0.5 * np.add(dF_loss_real, dF_loss_fake)
+
+                d_loss = 0.5 * np.add(dT_loss, dF_loss)
+
+                # Train Gens
+                g_loss = self.GANs[label].train_on_batch([img_A, img_B], [valid, valid, img_A, img_B, img_A, img_B])
+
+                print ("[Epoch %d/%d] [label: %d] [D loss: %f, acc: %3d%%] [G loss: %05f, adv: %05f, recon: %05f, id: %05f]" \
+                                                                        % ( epoch, epochs, label,
+                                                                            d_loss[0], 100*d_loss[1],
+                                                                            g_loss[0],
+                                                                            np.mean(g_loss[1:3]),
+                                                                            np.mean(g_loss[3:5]),
+                                                                            np.mean(g_loss[5:6]))
+        print("\n\n")
+
+
+            
+
+
